e0e7f2e833edc034a78f6bcf6b0864e0523c8b58
alliethe13 <alwa@adm.dtu.dk>
2022-03-21 10:15:51 +0000

    Del

--- R/data.R
+++ R/data.R
@@ -4,4 +4,9 @@
 #' 
 #' @format here is our table
 #' 
+<<<<<<< HEAD
 #' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+=======
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon table"
+>>>>>>> 297af9e93134ffef59d3686306f765a1aac507f3
