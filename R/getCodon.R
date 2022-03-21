c77e191a5af7a8c23ba6b5b9ba6d4ef4f873e5a8
alliethe13 <alwa@adm.dtu.dk>
2022-03-21 10:04:24 +0000

    Changes the getCodon Function

--- R/getCodon.R
+++ R/getCodon.R
@@ -1,15 +1,15 @@
 #' Getting Codons
 #'
-#' @param RNAseq RNA sequence input
+#' @param RNAseq RNA sequence input as string.
 #' @param start Position to start the codon counting/dividing. Default start = 1
 #'
-#' @return Goes through a RNA sequence and create the triplet codons (extracting character vector substring by 3)
-#' @export
+#' @return Character vector where each element is a codon.
+#' @export 
 #'
-#' @examples 
+#' @examples getCodon("UCGUUA", start = 1). Returns a character vector c("CGU", "UAA")
 getCodon <- function(RNASeq, start = 1){
   seqLen <- nchar(RNASeq)
-  codons <- substring(seqLen,
+  codons <- substring(RNASeq,
                       first = seq(from = start, to = seqLen-3+1, by = 3),
                       last = seq(from = 3+start-1, to = seqLen, by = 3))
   return(codons)
