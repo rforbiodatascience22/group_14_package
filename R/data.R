79035ab9cf489d3fcf8710c92733ecfbc7dc9f7d
Mads Hartmann <s184284@student.dtu.dk>
2022-03-24 20:37:44 +0000

    Fixed the datafile, the to lines that look like PAT's have been added to the Julia access token file

--- R/data.R
+++ R/data.R
@@ -3,16 +3,7 @@
 #' Description
 #'
 #' @format here is our table
-<<<<<<< HEAD
 #'
 #' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codonTable"
 
-=======
-#' 
-<<<<<<< HEAD
-#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
-=======
-#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
-"codon table"
->>>>>>> 297af9e93134ffef59d3686306f765a1aac507f3
->>>>>>> f606272b5398662db0c402c612e2a02d3e28b756
